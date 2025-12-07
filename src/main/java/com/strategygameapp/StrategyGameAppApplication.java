df3a158de8db9ad9267951420b551f0b7bd8dfb5
Antonia <antoniarata@yahoo.com>
2025-12-07 12:23:41 +0200

    adding CORS configuration and docker file for db

--- src/main/java/com/strategygameapp/StrategyGameAppApplication.java
+++ src/main/java/com/strategygameapp/StrategyGameAppApplication.java
@@ -2,10 +2,8 @@ package com.strategygameapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
 public class StrategyGameAppApplication {
 
     public static void main(String[] args) {
